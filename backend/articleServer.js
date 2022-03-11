d6cf7a288f7b099ebfe0b9a64a9d2767d1742f0a
Rus Miller <drm2@williams.edu>
2022-03-11 15:19:53 -0500

    Fullstack: Commented out static build.

--- backend/articleServer.js
+++ backend/articleServer.js
@@ -9,7 +9,9 @@ const port = process.env.PORT || 5000
 connectDB()
 const app = express()
 
-app.use( express.static( path.join( __dirname, '/src/build' ) ) )
+// Uncomment for the static build
+// app.use( express.static( path.join( __dirname, '/src/build' ) ) )
+
 app.use( express.json() )
 app.use( express.urlencoded( {extended: false} ) )
 
@@ -17,8 +19,9 @@ app.use( '/api/articles', require( './routes/articleRoutes' ) )
 
 app.use( errorHandler )
 
-app.get( '*', ( req, res ) => {
+// Uncomment for the static build
+/*app.get( '*', ( req, res ) => {
   res.sendFile( path.join( __dirname + '/src/build/index.html' ) )
-} )
+} )*/
 
 app.listen( port, () => console.log( `Server started on port ${port}!` ) )
